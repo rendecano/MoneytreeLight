794c2590c72c9939413796bc629da9e7931a46bd
Ren Decano <ireneo.decano@just-eat.com>
2019-10-21 17:20:04 +1100

    Initial commit

---
+++ app/src/main/java/io/rendecano/moneytreelight/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.rendecano.moneytreelight
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
